e223001b0f99c87721f52a6de13a23bee7bc6e21
VikzG <bouzemanej@yahoo.com>
2024-08-07 14:14:02 +0200

    add loader

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,33 @@
+"use client";
+
 import Hero from "@/components/Hero";
 import Grid from "@/components/Grid";
+import React, { useState, useEffect } from 'react';
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import RecentProjects from "@/components/RecentProjects";
 import { navItems } from "@/data";
 import Experiences from "@/components/Experiences";
 import Footer from "@/components/Footer";
+import Loader from "@/components/Loader";
 
 export default function Home() {
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2500);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
  <main className="relative bg-black-100 flex 
  justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
+        {loading ? (
+        <Loader />
+      ) : (
   <div className="max-w-7xl w-full">
     <FloatingNav navItems={navItems}
     />
@@ -19,6 +37,7 @@ export default function Home() {
     <Experiences />
     <Footer />
   </div>
+        )}
  </main>
   );
 }
